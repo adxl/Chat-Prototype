935ee170299c63dc4433170a5f53f5a1d35aba82
Adel Senhadji <adelsenhadjii@gmail.com>
2020-01-10 16:08:19 +0100

    init

---
+++ src/test/java/com/adxl/chat/ChatApplicationTests.java
@@ -0,0 +1,15 @@
+package com.adxl.chat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatApplicationTests
+{
+
+    @Test
+    void contextLoads()
+    {
+    }
+
+}
