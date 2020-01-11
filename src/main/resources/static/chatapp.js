dfcde800634f142e7a20ef9a9a84075796d26eba
Adel Senhadji <adelsenhadjii@gmail.com>
2020-01-11 17:18:54 +0100

    added manual connexion

--- src/main/resources/static/chatapp.js
+++ src/main/resources/static/chatapp.js
@@ -5,12 +5,37 @@ function connect() {
     var socket = new SockJS('/ws-chat-prototype');
     stompClient = Stomp.over(socket);
     stompClient.connect({}, function (frame) {
-        $("#messages").html("");
+        setConnected(true)
         console.log('Connected: ' + frame);
         stompClient.subscribe('/feed/response', function (message) {
             showMessage(JSON.parse(message.body).content);
         });
     });
+}
+
+function disconnect() {
+    if (stompClient !== null) {
+        stompClient.disconnect();
+    }
+    setConnected(false);
+    console.log("Disconnected");
+}
+
+function setConnected(connected) {
+    $("#login").prop("disabled", connected);
+    $("#disconnect").prop("disabled", !connected);
+
+    if (connected) {
+        $("#tableContainer").show();
+        $("#messageBar").show();
+        $("#disconnect").show();
+    }
+    else {
+        $("#messageBar").hide();
+        $("#tableContainer").hide();
+        $("#disconnect").hide();
+    }
+    $("#messages").html("");
 }
 
 function sendMessage() {
@@ -20,7 +45,7 @@ function sendMessage() {
 
 function showMessage(message) {
     var date = new Date();
-    var time = ('0'+date.getHours()).slice(-2) + ":" + ('0'+date.getMinutes()).slice(-2) + ":" + ('0'+date.getSeconds()).slice(-2);
+    var time = ('0'+date.getHours()).slice(-2) + ":" + ('0'+date.getMinutes()).slice(-2) /* + ":" + ('0'+date.getSeconds()).slice(-2)*/;
     $("#messages").append("<tr><td>" + "<span style='color: #6c757d'>"+time+"</span>" + " : " + message + "</td></tr>");
     document.getElementById('text').value='';
 }
@@ -29,5 +54,7 @@ $(function () {
     $("form").on('submit', function (e) {
         e.preventDefault();
     });
+    $( "#login" ).click(function() { connect(); });
+    $( "#disconnect" ).click(function() { disconnect(); });
     $( "#send" ).click(function() {sendMessage(); });
 });
